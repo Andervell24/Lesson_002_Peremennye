01508e9a6197b34d67ed8ae2d50dd7483d27dee7
Andervell24 <andervell24@gmail.com>
2024-08-25 07:18:06 +0300

    Add files via upload

---
+++ Lesson_002.py
@@ -0,0 +1,4 @@
+number_of_homework = 12
+number_of_hours_spent = 1.5
+course_name ='Курс:Python'
+print(course_name,',всего задач:12 ',number_of_homework,'затрачено часов:1,5 ',number_of_hours_spent,'среднее время выполнения 0.125 часа')
